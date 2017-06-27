a9aa903c99e293e0ae11ca86205c00ac9f38e17a
redfont <jrdumayag@gmail.com>
2017-06-27 00:53:36 -0400

    database\DDL.sql

--- database/DDL.sql
+++ database/DDL.sql
@@ -55,7 +55,7 @@ for each row set NEW.date_updated = CURRENT_TIMESTAMP;
 CREATE TABLE `redfin`.`contact_types` (
   `code` VARCHAR(50) NOT NULL,
   `description` VARCHAR(200) NULL,
-  `date_created` TIMESTAMP NULL,
+  `date_created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   `date_updated` TIMESTAMP NULL,
   `created_by` VARCHAR(100) NULL,
   `updated_by` VARCHAR(100) NULL,
