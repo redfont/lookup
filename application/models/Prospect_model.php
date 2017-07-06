652264a623e491801fbe84a63a1d0b4e3aa7113b
redfont <jrdumayag@gmail.com>
2017-07-06 18:03:52 +0800

    added prospects

---
+++ application/models/Prospect_model.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Prospect_model
+ *
+ * @author jrdumayag
+ */
+class Prospect_model extends CI_Model implements Common_DB_Operation {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+    
+    public function add($data) {
+        
+    }
+
+    public function delete($key) {
+        
+    }
+
+    public function get_all() {
+        
+    }
+
+    public function get_by_key($key) {
+        
+    }
+
+    public function update($data) {
+        
+    }
+
+//put your code here
+}
